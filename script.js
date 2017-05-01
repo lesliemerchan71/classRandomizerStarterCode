3c98c69c8ff72dc9dba38e541828306a551de356
Leslie Merchan <lesliemerchan71@gmail.com>
2017-05-01 12:33:59 +0000

    added some of the code still missing more

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-
+    var Students = ['Monserrat', 'Adonis', 'Leslie'];
+    var Teachers = ['Julia', 'Zack', 'Aaron', 'Justin', 'Alyxe'];
+    
+    var RandomOne = Math.floor(Math.random()*Students.length);
+    var RandomTwo = Math.floor(Math.random()*Teachers.length);
+    
+    console.log(Students[RandomOne]);
+    console.log(Teachers[RandomTwo]);
 
 
 });
