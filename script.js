c69be7156f051a8618ef7d099bb689a0057769d0
Leslie Merchan <lesliemerchan71@gmail.com>
2017-05-03 12:34:26 +0000

    tried appending

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
+    
     var Students = ['Monserrat', 'Adonis', 'Leslie'];
     var Teachers = ['Julia', 'Zack', 'Aaron', 'Justin', 'Alyxe'];
     
     var RandomOne = Math.floor(Math.random()*Students.length);
     var RandomTwo = Math.floor(Math.random()*Teachers.length);
     
-    console.log(Students[RandomOne]);
-    console.log(Teachers[RandomTwo]);
-
-
+    
+    $('#studentButton').click(function() {
+        var RandomOne = Math.floor(Math.random()*Students.length);
+        $("#studentDisplay").append(Students[RandomOne]);
+    }
+    
+    $('#teacherButton').click(function() {
+        var RandomTwo = Math.floor(Math.random()*Teachers.length);
+        $("#teacherDisplay").append(Teachers[RandomTwo]);
+    }
+    
+//change to jquery to do append//
 });
